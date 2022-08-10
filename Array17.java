6e34ab00afe5e6d07acc5533f0ae90da360d8f3d
Tiyasdey17 <105970777+Tiyasdey17@users.noreply.github.com>
2022-08-10 16:27:39 +0530

    Update Array17.java

--- Array17.java
+++ Array17.java
@@ -1,22 +1,23 @@
-public class Exercise33 {    
-   public static void main(String[] args) {
-        int nums[] = {20, 20, 30, 40, 50, 50, 50};  
-		System.out.println("Original array length: "+nums.length);
-		System.out.print("Array elements are: ");
-       for (int i = 0; i < nums.length; i++)
-        {
-            System.out.print(nums[i]+" ");
+import java.util.*; 
+import java.io.*; 
+ public class Exercise32 {
+ public static void main(String[] args)
+ {
+    int[] array_nums = {77, 77, 12, 23, 65, 77};
+	System.out.println("Original Array: "+Arrays.toString(array_nums)); 
+	int num1 = 12;
+    int num2 = 23;
+	
+	System.out.println("Result: "+result(array_nums, num1, num2));
+    }	
+  
+  public static boolean result(int[] array_nums, int num1, int num2) {
+    for (int number : array_nums) {
+      boolean r = number != num1 && number != num2;
+      if (r) {
+        return false;
         }
-		System.out.println("\nThe new length of the array is: "+array_sort(nums));
-			
-    }
-    
-    public static int array_sort(int[] nums) {
-         int index = 1;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[index-1])
-                nums[index++] = nums[i];
-        }
-	  return index;
-    }
+     }
+        return true;
+   }
 }
